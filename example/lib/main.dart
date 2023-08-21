cfa9fb93b28c01a8c20bc0ea65be7d939025c438
Filipp Dmitruk <filippdmitruk@coherentsolutions.com>
2023-08-22 01:05:11 +0200

    CalendarContainer update (Animation moved there)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/intl.dart';
 
 void main() {
-  // timeDilation = 10.0;
+  // timeDilation = 5.0;
   runApp(const MyApp());
 }
 
@@ -54,266 +54,7 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _controller;
-  late final Animation<double> opacity;
-  late final Animation<double> scale;
-  late final Animation<double> height;
-
-  bool _isExpanded = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 300),
-    );
-
-    scale = TweenSequence<double>([
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 0.29,
-          end: 0.39,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 0.39,
-          end: 0.5,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 0.5,
-          end: 0.61,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 0.61,
-          end: 0.7,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 0.7,
-          end: 0.785,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 0.785,
-          end: 0.85,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 0.85,
-          end: 0.9,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 0.9,
-          end: 0.94,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 0.94,
-          end: 0.965,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 0.965,
-          end: 0.985,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 0.985,
-          end: 0.997,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 0.997,
-          end: 1.0,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 1.0,
-          end: 1.01,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 1.01,
-          end: 1.0,
-        ),
-        weight: 0.071,
-      ),
-    ]).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Curves.linear,
-      ),
-    );
-
-    height = TweenSequence<double>([
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 75.0,
-          end: 110.0,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 110.0,
-          end: 150.0,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 150.0,
-          end: 185.0,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 185.0,
-          end: 220.0,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 220.0,
-          end: 245.0,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 245.0,
-          end: 265.0,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 265.0,
-          end: 283.0,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 283.0,
-          end: 295.0,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 295.0,
-          end: 305.0,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 305.0,
-          end: 310.0,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 310.0,
-          end: 315.0,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 315.0,
-          end: 318.0,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 318.0,
-          end: 319.0,
-        ),
-        weight: 0.071,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 319.0,
-          end: calendarHeight,
-        ),
-        weight: 0.071,
-      ),
-    ]).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Curves.linear,
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _toggleAnimation() {
-    if (_isExpanded) {
-      _controller.reverse();
-    } else {
-      _controller.forward();
-    }
-    setState(() {
-      _isExpanded = !_isExpanded;
-    });
-
-    if (_isExpanded) {
-      _width = calendarWidth;
-      _height = calendarHeight;
-    } else {
-      _width = calendarWidth - 3.0;
-      _height = 75.0;
-    }
-
-    setState(() {});
-  }
-
-  double _width = calendarWidth;
-  double _height = calendarHeight;
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     final nowDate = DateTime.now();
@@ -323,36 +64,23 @@ class _MyHomePageState extends State<MyHomePage>
       body: Stack(
         children: [
           Positioned(
-            bottom: 211.0,
-            right: 30.0,
-            child: AnimatedBuilder(
-              animation: _controller,
-              child: CupertinoCalendar(
-                minimumDate: nowDate.subtract(const Duration(days: 15)),
-                initialDate: nowDate,
-                currentDate: nowDate,
-                maximumDate: DateTime(2030, 02, 14),
-                onDateChanged: (_) {},
-              ),
-              builder: (context, child) {
-                return Transform.scale(
-                  scale: scale.value,
-                  alignment: const Alignment(0.5, 1.0),
-                  child: SizedBox(
-                    height: height.value,
-                    child: child,
-                  ),
-                );
-              },
+            bottom: 225.0,
+            right: 22.0,
+            child: CupertinoCalendar(
+              minimumDate: nowDate.subtract(const Duration(days: 15)),
+              initialDate: nowDate,
+              currentDate: nowDate,
+              maximumDate: DateTime(2030, 02, 14),
+              onDateChanged: (_) {},
             ),
           ),
           Positioned(
-            top: 500.0,
+            top: 450.0,
             bottom: 0.0,
             left: 0.0,
             right: 0.0,
             child: GestureDetector(
-              onTap: _toggleAnimation,
+              // onTap: _toggleAnimation,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
--- lib/src/calendar/calendar_container.dart
+++ lib/src/calendar/calendar_container.dart
@@ -1,7 +1,7 @@
 import 'package:cupertino_calendar/lib.dart';
 import 'package:flutter/cupertino.dart';
 
-class CalendarContainer extends StatelessWidget {
+class CalendarContainer extends StatefulWidget {
   const CalendarContainer({
     required this.child,
     required this.decoration,
@@ -12,26 +12,308 @@ class CalendarContainer extends StatelessWidget {
   final CalendarContainerDecoration decoration;
 
   @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: calendarWidth,
-      height: calendarHeight,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: decoration.borderRadius,
-          color: decoration.backgroundColor,
-          boxShadow: decoration.boxShadow,
+  State<CalendarContainer> createState() => _CalendarContainerState();
+}
+
+class _CalendarContainerState extends State<CalendarContainer>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _mainController;
+
+  late Animation<double> opacity;
+  late Animation<double> scale;
+  late Animation<double> height;
+
+  @override
+  void initState() {
+    super.initState();
+    _mainController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 410),
+      reverseDuration: const Duration(milliseconds: 250),
+    );
+
+    opacity = TweenSequence<double>(<TweenSequenceItem<double>>[
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 0.0,
+          end: 1.0,
         ),
-        clipBehavior: Clip.hardEdge,
-        child: FittedBox(
-          alignment: Alignment.topCenter,
-          fit: BoxFit.none,
-          child: SizedBox(
-            width: calendarWidth,
-            height: calendarHeight,
-            child: child,
+        weight: 1.0,
+      ),
+    ]).animate(
+      CurvedAnimation(
+        parent: _mainController,
+        curve: Curves.ease,
+      ),
+    );
+
+    scale = TweenSequence<double>(<TweenSequenceItem<double>>[
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 0.22,
+          end: 0.39,
+        ),
+        weight: 0.03846,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 0.29,
+          end: 0.39,
+        ),
+        weight: 0.03846,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 0.39,
+          end: 0.5,
+        ),
+        weight: 0.03846,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 0.5,
+          end: 0.61,
+        ),
+        weight: 0.03846,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 0.61,
+          end: 0.7,
+        ),
+        weight: 0.03846,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 0.7,
+          end: 0.785,
+        ),
+        weight: 0.03846,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 0.785,
+          end: 0.85,
+        ),
+        weight: 0.03846,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 0.85,
+          end: 0.9,
+        ),
+        weight: 0.03846,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 0.9,
+          end: 0.94,
+        ),
+        weight: 0.03846,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 0.94,
+          end: 0.965,
+        ),
+        weight: 0.03846,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 0.965,
+          end: 0.985,
+        ),
+        weight: 0.03846,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 0.985,
+          end: 0.997,
+        ),
+        weight: 0.03846,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 0.997,
+          end: 1.005,
+        ),
+        weight: 0.03846,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 1.005,
+          end: 1.01,
+        ),
+        weight: 0.25,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 1.01,
+          end: 1.0,
+        ),
+        weight: 0.25,
+      ),
+    ]).animate(_mainController);
+
+    height = TweenSequence<double>(<TweenSequenceItem<double>>[
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 70.0,
+          end: 110.0,
+        ),
+        weight: 0.04166,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 75.0,
+          end: 110.0,
+        ),
+        weight: 0.04166,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 110.0,
+          end: 150.0,
+        ),
+        weight: 0.04166,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 150.0,
+          end: 185.0,
+        ),
+        weight: 0.04166,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 185.0,
+          end: 220.0,
+        ),
+        weight: 0.04166,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 220.0,
+          end: 245.0,
+        ),
+        weight: 0.04166,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 245.0,
+          end: 265.0,
+        ),
+        weight: 0.04166,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 265.0,
+          end: 283.0,
+        ),
+        weight: 0.04166,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 283.0,
+          end: 295.0,
+        ),
+        weight: 0.04166,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 295.0,
+          end: 305.0,
+        ),
+        weight: 0.04166,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 305.0,
+          end: 310.0,
+        ),
+        weight: 0.04166,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 310.0,
+          end: 315.0,
+        ),
+        weight: 0.04166,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 315.0,
+          end: 319.0,
+        ),
+        weight: 0.25,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(
+          begin: 319.0,
+          end: calendarHeight,
+        ),
+        weight: 0.25,
+      ),
+    ]).animate(_mainController);
+  }
+
+  @override
+  void dispose() {
+    _mainController.dispose();
+    super.dispose();
+  }
+
+  bool _isExpanded = false;
+
+  void _toggleAnimation() {
+    if (_isExpanded) {
+      _mainController.reverse();
+    } else {
+      _mainController.forward();
+    }
+
+    setState(() {
+      _isExpanded = !_isExpanded;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _toggleAnimation,
+      child: AnimatedBuilder(
+        animation: _mainController,
+        child: SizedBox(
+          width: calendarWidth,
+          height: calendarHeight,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: widget.decoration.borderRadius,
+              color: widget.decoration.backgroundColor,
+              boxShadow: widget.decoration.boxShadow,
+            ),
+            clipBehavior: Clip.hardEdge,
+            child: FittedBox(
+              alignment: Alignment.topCenter,
+              fit: BoxFit.none,
+              child: SizedBox(
+                width: calendarWidth,
+                height: calendarHeight,
+                child: widget.child,
+              ),
+            ),
           ),
         ),
+        builder: (BuildContext context, Widget? child) {
+          return Transform.scale(
+            scale: scale.value,
+            alignment: const Alignment(0.6, 1.0),
+            child: SizedBox(
+              height: height.value,
+              child: child,
+            ),
+          );
+        },
       ),
     );
   }
